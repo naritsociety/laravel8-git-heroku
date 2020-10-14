d03d3644b062d6f4c6d7f4046e783212a8d4f5f2
Narit Charoenval <naritsociety@gmail.com>
2020-10-14 23:32:18 +0700

    Prototype

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div class="container">
-            <h3>Laravel8 on Heroku</h3>
+            <h3>Laravel8 on Heroku With ReactJS Prototype</h3>
             <div id="root"></div>
         </div>
         <script src="{{ asset('js/app.js')}}"></script>
